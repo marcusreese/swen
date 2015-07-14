e9af39c1e3784ad0499eb1c989c7aa7c531293b2
Marcus Reese <marcusoccasional@gmail.com>
2015-07-14 16:57:06 -0500

    Started integration tests

--- tests/jasmine/client/integration/modeServiceIntegrationSpec.js
+++ tests/jasmine/client/integration/modeServiceIntegrationSpec.js
@@ -1,23 +1,15 @@
-// modeServiceIntegrationSpec.js
-/* Removing as I get "ReferenceError: module is not defined".
-	Instead I'm doing these tests as unit tests, where it's working.
-	My justification: modeService is a unit.
-		So integration can include other services and controller etc.
-describe('In modeService, the browse mode', function() {
-        beforeEach(module("swen"));
-        // Access the real ModeService as $modeService.
-        var $modeService = {};
-        beforeEach(inject(function (_modeService_) {
-                $modeService = _modeService_;
-        }));
-        it("is listed first", function() {
-                var modesArray = $modeService.getModes();
-                expect(modesArray[0].codeValue).toBe("browse");
+// browseIntegrationSpec.js
+"use strict";
+describe('When page is loaded,', function(done) {
+beforeEach(angular.mock.module("swen"));
+        it("the default route is '/mjr:welcome'.", function() {
+                expect(location.pathname).toBe("/mjr:welcome");
         });
-        it("is the default", function() {
-                var modeString = $modeService.getDefaultMode();
-                expect(modeString).toBe("browse");
+        it("there should be at least 2 subpages.", function() {
+		expect($(".panel").children().length).not.toBeLessThan(2);
+        });
+        it("there should be posts.", function() {
+
         });
 
 });
-*/
